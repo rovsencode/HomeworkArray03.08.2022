7966985633c7ca0a2cdbf02a4d5817dadb74e47e
rovsencode <rovshanakh@code.edu.az>
2022-08-03 13:34:28 +0400

    homework array

---
+++ HomeworkArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeworkArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 24, 16, 19, 23, 28, 34, 39, 124, 128, 234,2, 256, 643 };
+            int min=array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (min>array[i])
+                {
+                    min = array[i];
+
+                }
+
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
